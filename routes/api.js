6e1d5ad2fa8ceb990450115395c69b84a9b44eb4
Javier <javier.sardinas@gmail.com>
2022-05-31 23:51:00 -0500

    Organize routes

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,15 @@
-const express = require('express');
-const path = require('path');
+const api = require('express').Router();
 const fs = require('fs');
-var notesData = require('./db/db.json')
 const { v4: uuidv4 } = require('uuid');
+var notesData = require('../db/db.json');
 
-const PORT = 5001;
+api.get('/notes', (req, res) => res.json(notesData));
 
-const app = express();
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
-app.get('/api/notes', (req, res) => res.json(notesData));
-
-app.post('/api/notes', (req, res) => {
+api.post('/notes', (req, res) => {
   if (req.body && req.body.title && req.body.text){
     const {title, text} = req.body;
     notesData.push({"id":uuidv4(), "title":title, "text":text});
-    fs.writeFile('./db/db.json', JSON.stringify(notesData), err => {
+    fs.writeFile('../db/db.json', JSON.stringify(notesData), err => {
       if(err){
         notesData.pop();
         console.log(err);
@@ -38,7 +27,7 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-app.delete('/api/notes/:id', (req, res) => {
+api.delete('/notes/:id', (req, res) => {
   console.log(req.params.id);
   var i = 0;
   while(i < notesData.length && notesData[i].id != req.params.id)++i;
@@ -47,7 +36,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
   const deletedNote = notesData[i];
   notesData.splice(i,1);  
-  fs.writeFile('./db/db.json', JSON.stringify(notesData), err => {
+  fs.writeFile('../db/db.json', JSON.stringify(notesData), err => {
     if (err){
       console.log(err);
       notesData.push(deletedNote);
@@ -61,16 +50,4 @@ app.delete('/api/notes/:id', (req, res) => {
   });
 });
 
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+module.exports = api;
